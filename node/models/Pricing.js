8b493ee37e8fb85c0e42fef4c1e9558a7271dc94
yorktronic <ty.walker@gmail.com>
2017-03-02 23:58:39 +0000

    update after server was configured and restarted

--- node/models/Pricing.js
+++ node/models/Pricing.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 // mongodb schema for alan's exercise
 var PricingSchema = new mongoose.Schema({
 	vendor: String,
+	zone: String,
 	priceType: String,
 	instanceType: String,
 	productDescription: String,
